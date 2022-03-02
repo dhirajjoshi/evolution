ca66a210793ee14f9ae438d0b0047936dc2b8fe2
Dhiraj joshi <56167665+dhirajjoshi@users.noreply.github.com>
2022-03-02 17:48:50 +0530

    ngStyle

--- shopping/src/app/product/product/listing/listing.component.ts
+++ shopping/src/app/product/product/listing/listing.component.ts
@@ -9,5 +9,15 @@ export class ListingComponent {
 
   constructor() { }
 @Input()product=[]
-
+col(q){
+  if(q==0){
+    return {'background-color':'#ff0000','color':'#FFFFFF'}
+  }
+  else if(q>0 && q<50){
+    return {'background-color':'#ffA500','color':'#FFFFFF'}
+  }
+  else if(q>50){
+    return {'background-color':'#228b22','color':'#FFFFFF'}
+  }
+}
 }
