6f7a3d6032577a60a309fd63a863a31b51787961
Dhiraj joshi <56167665+dhirajjoshi@users.noreply.github.com>
2022-03-02 17:04:45 +0530

    data display

--- shopping/src/app/product/product/product.component.ts
+++ shopping/src/app/product/product/product.component.ts
@@ -1,24 +1,24 @@
 import { Component, ElementRef, ViewChild} from '@angular/core';
-interface Product{
-  name:string,
-  quan:number,
-  color:string,
-  lot:number,
-  desc:string,
-  rate:number}
+// interface Product{
+//   name:string,
+//   quan:number,
+//   color:string,
+//   lot:number,
+//   desc:string,
+//   rate:number}
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements Product{
-  public name: string;
-  public quan: number;
-  public color: string;
-  public lot: number;
-  public desc: string;
-  public rate: number;
+export class ProductComponent{
+  // public name: string;
+  // public quan: number;
+  // public color: string;
+  // public lot: number;
+  // public desc: string;
+  // public rate: number;
 
   @ViewChild('rate',{
     static:false
@@ -43,16 +43,30 @@ export class ProductComponent implements Product{
   @ViewChild('desc',{
     static:false
   })productDescription!:ElementRef
-//product={name:'',quan:'',color:'',lot:'',desc:'',rate:''}
-  data(event:Event){
-    this.name=this.productName.nativeElement.value
-    this.quan=this.productQuantity.nativeElement.value
-    this.color=this.productColor.nativeElement.value
-    this.lot=this.productLot.nativeElement.value
-    this.desc=this.productDescription.nativeElement.value
-    this.rate=this.productRate.nativeElement.value
-    //this.product.name=this.productName.nativeElement.value
+product=[]
+  data(){
+    // this.name=this.productName.nativeElement.value
+    // this.quan=this.productQuantity.nativeElement.value
+    // this.color=this.productColor.nativeElement.value
+    // this.lot=this.productLot.nativeElement.value
+    // this.desc=this.productDescription.nativeElement.value
+    // this.rate=this.productRate.nativeElement.value
+    
+    //this.product.lot=this.productLot.nativeElement.value
+    
+    // this.product.name=this.productName.nativeElement.value
+    // this.product.quan=this.productQuantity.nativeElement.value
+    // this.product.color=this.productColor.nativeElement.value
+    // this.product.desc=this.productDescription.nativeElement.value
+    // this.product.rate=this.productRate.nativeElement.value
+
+    let temp={
+      'name':this.productName.nativeElement.value,
+      'quan':this.productQuantity.nativeElement.value,
+      'color':this.productColor.nativeElement.value,
+      'desc':this.productDescription.nativeElement.value,
+      'rate':this.productRate.nativeElement.value
+    }
+    this.product.push(temp)
   }
-  
-  
 }
